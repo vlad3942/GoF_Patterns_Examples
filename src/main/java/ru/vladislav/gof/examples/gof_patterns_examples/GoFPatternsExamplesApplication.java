09833b32a92e50cbfb3328f012045e93464a146d
vlad3942 <vladikgud@mail.ru>
2024-09-08 18:37:27 +0400

    Init commit

---
+++ src/main/java/ru/vladislav/gof/examples/gof_patterns_examples/GoFPatternsExamplesApplication.java
@@ -0,0 +1,13 @@
+package ru.vladislav.gof.examples.gof_patterns_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFPatternsExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoFPatternsExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vladislav/gof/examples/gof_patterns_examples/GoFPatternsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladislav.gof.examples.gof_patterns_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoFPatternsExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
